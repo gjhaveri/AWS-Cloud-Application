a6be09b8fc014415fec906725b6adbdf43652ada
gjhaveri <itsmeasgaurav@gmail.com>
2016-10-09 18:01:18 -0500

    Add files via upload

---
+++ create-env.sh
@@ -0,0 +1,9 @@
+#!/bin/bash 
+echo "Let us Create a world of Cloud"
+echo "Let us first create a Load Balancer named gjhaveri"
+aws elb create-load-balancer --load-balancer-name gjhaveri --listeners Protocol=Http,LoadBalancerPort=80,InstanceProtocol=Http,InstancePort=80 --subnets subnet-b36445d7
+echo "Creating Launch Configuration named ubuntuserver"
+aws autoscaling create-launch-configuration --launch-configuration-name ubuntuserver --image-id ami-06b94666 --key-name test --security-groups sg-b5488acc --instance-type t2.micro --user-data  file://installenv.sh
+echo "Finally let's create an auto-scaling-group named serverdemo"
+aws autoscaling create-auto-scaling-group --auto-scaling-group-name serverdemo --launch-configuration-name ubuntuserver --load-balancer-names gjhaveri --min-size 1 --max-size 5 --desired-capacity 3 --vpc-zone-identifier subnet-b36445d7
+echo "Don't forget to kill everything else jhajek will not help you to get your money!! Boom and out of here..."
