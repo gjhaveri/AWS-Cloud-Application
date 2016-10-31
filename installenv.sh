e83417a1d3d5f8cc7ae437847e8f1eaedc453b7a
gjhaveri <itsmeasgaurav@gmail.com>
2016-10-30 19:20:22 -0500

    Updating Previous Incorrect Cloning

--- installenv.sh
+++ installenv.sh
@@ -3,6 +3,8 @@
 sudo apt-get update
 sudo apt-get -y install git 
 sudo apt-get -y install apache2
-
 sudo systemctl enable apache2
 sudo systemctl start apache2
+cd /home/ubuntu
+sudo git clone https://github.com/gjhaveri/boostrap-website.git
+sudo mv /home/ubuntu/boostrap-website/* /var/www/html
