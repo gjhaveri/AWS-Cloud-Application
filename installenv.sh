75ee0e420cf7ae085a2700912f51dbc76d73ad9d
gjhaveri <itsmeasgaurav@gmail.com>
2016-11-06 18:01:44 -0600

    Update

--- installenv.sh
+++ installenv.sh
@@ -1,31 +1,23 @@
-
-
-#sudo apt-get update
-#sudo apt-get -y install git 
-#sudo apt-get -y install apache2
-#sudo systemctl enable apache2
-#sudo systemctl start apache2
-#cd /home/ubuntu
-#sudo git clone https://github.com/gjhaveri/boostrap-website.git
-#sudo mv /home/ubuntu/boostrap-website/* /var/www/html
-
-
 #!/bin/bash
 
 git clone git@github.com:illinoistech-itm/gjhaveri.git /home/ubuntu/gjhaveri
 
 sudo apt-get update
 sudo apt-get -y install git apache2 php-xml libapache2-mod-php php php-mysql curl php-curl zip unzip
+
+export COMPOSER_HOME=/root && /usr/bin/composer.phar self-update 1.0.0-alpha11
+
 sudo curl -sS https://getcomposer.org/installer | php
 
 sudo php composer.phar require aws/aws-sdk-php
 sudo systemctl enable apache2
 sudo systemctl start apache2
+	
+sudo mv vendor /var/www/html/
 
-sudo mv vendor /var/www/html
-cd /var/www/html
-sudo rm index.html
+sudo rm index.html /var/www/html/
 
 sudo git clone https://github.com/illinoistech-itm/gjhaveri
 
-sudo mv /home/ubuntu/gjhaveri/* /var/www/html
+sudo mv /home/ubuntu/gjhaveri/*.php	 /var/www/html/
+#git@github.com:illinoistech-itm/gjhaveri.git /home/ubuntu/gjhaveri
