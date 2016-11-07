10c7e868f1da07fcbdb0d353f50e79523697d35d
gjhaveri <itsmeasgaurav@gmail.com>
2016-11-06 21:21:24 -0600

    Database changed to school and table to students

--- dbtest.php
+++ dbtest.php
@@ -1,22 +1,28 @@
 <?php
 
 echo "Hello world";
-$link = mysqli_connect("clouddatabases.clbbdifdgtxm.us-west-2.rds.amazonaws.com:3306","awsdatabase","awsdatabase","gjhaverirds") or die("Error" . mysqli_error($link));
+$link = mysqli_connect("clouddatabases.clbbdifdgtxm.us-west-2.rds.amazonaws.com:3306","awsdatabase","awsdatabase","school") or die("Error" . mysqli_error($link));
 
 //echo "Here is the result: " . $link;
 
 
-$sql = "CREATE TABLE school
+$sql1 = "CREATE DATABASE IF NOT EXISTS school";
+if ($link->query($sql1) === TRUE) {
+echo "Database created successfully";
+} else {
+echo "Error creating database: " . $link->error;
+}
+
+$sql = "CREATE TABLE students
 (
 ID INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
-PosterName VARCHAR(32),
 Name VARCHAR(255),
 Age int(3)
 )";
 
 $link->query($sql);
 
-$sql2 = "INSERT INTO `school` (`name`,`age`) VALUES ('gj',8),('Pc',99),('Jam',7),('Ze',6),('Alem',12)";
+$sql2 = "INSERT INTO `students` (`name`,`age`) VALUES ('gaurav',21),('matthew',29),('alberto',27),('Zalewski',26),('Alem',32)";
 if ($link->query($sql2) === TRUE) {
 echo "Data inserted successfully";
 } else {
@@ -24,7 +30,7 @@ echo "Error creating database: " . $link->error;
 }
 
 
-$retrieve="SELECT * FROM school";
+$retrieve="SELECT * FROM students";
 #echo "Data is here" . $retrieve;
 
 $result= mysqli_query($link,$retrieve);
@@ -37,5 +43,11 @@ if ($result->num_rows > 0) {
     }
 }
 $link->close();
+
+ $result1 = mysqli_query('TRUNCATE TABLE school');
+
+ if ($result1) {
+   echo "Request ID table has been truncated";
+ }
 
 ?>
