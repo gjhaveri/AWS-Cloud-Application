c54bb12a92f0fd6c2daa0980caaae8850babd8b8
gjhaveri <itsmeasgaurav@gmail.com>
2016-11-07 00:00:20 -0600

    Editing

--- install-env.sh
+++ install-env.sh
@@ -14,4 +14,3 @@ aws autoscaling create-launch-configuration --launch-configuration-name $4 --ima
 echo "Finally let's create an auto-scaling-group named serverdemo"
 aws autoscaling create-auto-scaling-group --auto-scaling-group-name serverdemo --launch-configuration-name ubuntuserver --load-balancer-names gjhaveri --min-size 1 --max-size 5 --desired-capacity $5 --vpc-zone-identifier subnet-b36445d7
 echo "Don't forget to kill everything else jhajek will not help you to get your money!! Boom and out of here..."
-
