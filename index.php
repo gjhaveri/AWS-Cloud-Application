8e529d2413f5c89b453f83d30fa926bb8c215e03
gjhaveri <itsmeasgaurav@gmail.com>
2016-11-17 09:19:01 -0600

    Modifying the web page

--- index.php
+++ index.php
@@ -32,3 +32,24 @@ foreach ($result['Buckets'] as $bucket) {
 </form>
 </body>
 </html>
+
+#<?php session_start(); ?>
+#<html>
+#<head><title>Hello app</title>
+#</head>
+#<body>
+
+#<!-- The data encoding type, enctype, MUST be specified as below -->
+#<form enctype="multipart/form-data" action="welcome.php" method="POST">
+ #   <!-- MAX_FILE_SIZE must precede the file input field -->
+  #  <input type="hidden" name="MAX_FILE_SIZE" value="3000000" />
+#    <!-- Name of input element determines name in $_FILES array -->
+#    Send this file: <input name="userfile" type="file" /><br />
+#Enter Email of user: <input type="email" name="email"><br />
+#Enter Phone of user (1-XXX-XXX-XXXX): <input type="phone" name="phone">
+
+#<input type="submit" value="Send File" />
+#</form>
+#<hr />
+#</body>
+#</html>
