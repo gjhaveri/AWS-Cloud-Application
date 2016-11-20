3b512e115e0ce63322f00d4d3691dfc970adabaa
gjhaveri <itsmeasgaurav@gmail.com>
2016-11-19 20:07:06 -0600

    Uploading files within temp folder

--- uploader.php
+++ uploader.php
@@ -10,17 +10,36 @@ $s3 = new Aws\S3\S3Client([
 
 
 
-// using Session Variable
+// have to hard code this here because index.php doesn't exist
 $_SESSION['email']=$_SESSION['usernames'];
 
-echo "\n" . $_SESSION['email'] ."\n";
+echo "\n Welcome to Uploader Page \n" . $_SESSION['email'] ."\n";
+
+// To upload the file and giving temporary name.
+
+
+$uploaddir = '/tmp/';
+$uploadfile = $uploaddir . basename($_FILES['userfile']['name']);
+
+#echo '<pre>';
+if (move_uploaded_file($_FILES['userfile']['tmp_name'], $uploadfile)) {
+    echo "<br>File is valid, and was successfully uploaded.\n";
+} else {
+    echo "<br>Possible file upload attack!\n";
+}
+
+echo '<br>Here is some more debugging info:';
+print_r($_FILES);
+
+
+
 ?>
 
 <html>
 <head><title>Hello app</title>
 </head>
 <body>
-Hello world
+
 <hr />
 <a href="admin.php"> Admin </a>
 
