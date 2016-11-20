e328a512978c67d1981a62fc25f246a4c8150163
gjhaveri <itsmeasgaurav@gmail.com>
2016-11-19 19:33:53 -0600

    Changing the visibility based on user

--- welcome.php
+++ welcome.php
@@ -1,20 +1,27 @@
 <?php
 session_start();
 echo $username . "\n";
-
 $email = $_SESSION['usernames'];
-
 echo "<b>Hello Cloud User, you are Welcomed to this world</b><br>" . "\n";
-echo "<br>Your email is: " . $email . "\n";
-
+echo "<br>Your email is:" . $email . "\n" . "<br>";
 $_SESSION['userid'] = $_POST['userid'];
+if ($_SESSION['usernames']!="jhajek@iit.edu")
+{
 ?>
+
+<html>
+<head><body>
+<br><a href="gallery.php"> Gallery </a> | <a href="upload.php"> Upload </a> | <a href="uploader.php"> Uploader </a> | <a href="admin.php"> Admin </a> | <a href= "logout.php"> logout </a>
+</head></body></html>
+
+<?php
+}
+?>
+
 
 <html>
 <head><title>Hello app</title>
 </head>
 <body>
 <hr />
-<a href="gallery.php"> Gallery </a> | <a href="upload.php"> Upload </a> | <a href="Uploader.php"> Uploader </a> | <a href="admin.php"> Admin </a>
-
 </body></html>
