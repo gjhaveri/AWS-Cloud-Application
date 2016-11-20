962d9e0f8394db244804d675887fdd4be1569c39
gjhaveri <itsmeasgaurav@gmail.com>
2016-11-20 12:12:06 -0600

    Adding Further Functionality

--- uploader.php
+++ uploader.php
@@ -52,3 +52,59 @@ $rdsclient = new Aws\Rds\RdsClient([
   'region'            => 'us-west-2',
     'version'           => 'latest'
 ]);
+
+$rdsresult = $rdsclient->describeDBInstances([
+    'DBInstanceIdentifier' => 'clouddatabases'
+]);
+
+
+//$endpoint = $rdsresult['DBInstances'][0]['Endpoint']['Address'];
+//echo $endpoint . "\n";
+
+$link = mysqli_connect("clouddatabases.clbbdifdgtxm.us-west-2.rds.amazonaws.com:3306","awsdatabase","awsdatabase","school") or die("Error " . mysqli_error($link));
+/* check connection */
+if (mysqli_connect_errno()) {
+    printf("Connect failed: %s\n", mysqli_connect_error());
+    exit();
+}
+
+// code to insert new record
+/* Prepared statement, stage 1: prepare */
+
+if (!($stmt = $link->prepare("INSERT INTO records (id, email, phone, s3-raw-url, s3-finished-url, status,reciept) VALUES (NULL,?,?,?,?,?,?)"))) {
+    echo "Prepare failed: (" . $mysqli->errno . ") " . $mysqli->error;
+}
+
+
+$email = $_SESSION['email'];
+$phone = '2248177955';
+$s3finishedurl = ' ';
+$s3rawurl = ' ';
+$status = 0;
+$receipt = md5($url);
+// prepared statements will not accept literals (pass by reference) in bind_params, you need to declare variables
+//$stmt->bind_param("ssssii",$email,$phone,$s3rawurl,$s3finishedurl,$status,$receipt);
+
+$stmt->bind_param("ssssii",$email,$phone,$s3rawurl,$s3finsihedurl,$status,$reciept);
+
+
+if (!$stmt->execute()) {
+            echo "Execute failed: (" . $stmt->errno . ") " . $stmt->error;
+}
+
+printf("%d Row inserted.\n", $stmt->affected_rows);
+
+/* explicit close recommended */
+//$stmt->close();
+
+
+?>
+
+<html>
+<head><title>Hello app</title>
+</head>
+<body>
+<hr />
+<a href="admin.php"> Admin </a>
+</body></html>
+
