752949c9eaa4e7b3dd13d7eadc55f2e10e17784f
gjhaveri <itsmeasgaurav@gmail.com>
2016-11-20 14:58:03 -0600

    Displaying records from database

--- gallery.php
+++ gallery.php
@@ -1,22 +1,44 @@
-
 <?php
 
 session_start();
 
 echo "Your email is: " .  $_SESSION['email'];
 $email = $_SESSION['email'];
-echo "\n" . md5($email);
+echo "\n<br>" . md5($email);
 
 $_SESSION['receipt'] = md5($email);
+
+require 'vendor/autoload.php';
+
+
+$rdsclient = new Aws\Rds\RdsClient([
+  'region'            => 'us-west-2',
+    'version'           => 'latest'
+]);
+$rdsresult = $rdsclient->describeDBInstances([
+    'DBInstanceIdentifier' => 'clouddatabases'
+]);
+//$endpoint = $rdsresult['DBInstances'][0]['Endpoint']['Address'];
+//echo $endpoint . "\n";
+$link = mysqli_connect("clouddatabases.clbbdifdgtxm.us-west-2.rds.amazonaws.com:3306","awsdatabase","awsdatabase","school") or die("Error " . mysqli_error($link));
+/* check connection */
+if (mysqli_connect_errno()) {
+    printf("Connect failed: %s\n", mysqli_connect_error());
+    exit();
+}
+
+$link->real_query("SELECT * FROM recordings");
+
+$res = $link->use_result();
+echo "Result set order...\n";
+
+while ($row = $res->fetch_assoc()) {
+    echo "<img src =\" " . $row['s3rawurl'] . "\" /><img src =\"" .$row['s3finishedurl'] . "\"/>";
+echo $row['id'];
+}
+
+$link->close();
+
+
 ?>
-
-<html>
-<head><title>Hello app</title>
-</head>
-<body>
-Gallery PHP
-<a href="gallery.php"> Gallery </a> <a href="upload.php"> Upload </a>
-
-</body></html>
-
 
