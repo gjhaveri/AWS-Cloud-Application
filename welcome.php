8d3eb7737c7b86761a3f44d6c3a22ad8c869d54a
gjhaveri <itsmeasgaurav@gmail.com>
2016-11-21 00:05:13 -0600

    Adding HTML Code

--- welcome.php
+++ welcome.php
@@ -5,12 +5,15 @@ $email = $_SESSION['usernames'];
 echo "<b>Hello Cloud User, you are Welcomed to this world</b><br>" . "\n";
 echo "<br>Your email is:" . $email . "\n" . "<br>";
 $_SESSION['userid'] = $_POST['userid'];
-if ($_SESSION['usernames']!="jhajek@iit.edu")
+if ($_SESSION['usernames']!="gjhaveri@hawk.iit.edu")
 {
 ?>
 
 <html>
 <head><body>
+<link href="styles.css" rel="stylesheet" type="text/css">
+
+
 <br><a href="gallery.php"> Gallery </a> | <a href="upload.php"> Upload </a> | <a href="uploader.php"> Uploader </a> | <a href="admin.php"> Admin </a> | <a href= "logout.php"> logout </a>
 </head></body></html>
 
