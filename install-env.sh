a69d73601cc4f2febcdd13a68be489da8791c7e6
gjhaveri <itsmeasgaurav@gmail.com>
2016-12-03 22:49:51 -0600

    Processing Instance with Cron Job

--- install-env.sh
+++ install-env.sh
@@ -14,7 +14,4 @@ aws autoscaling create-launch-configuration --launch-configuration-name $4 --ima
 echo "Finally let's create an auto-scaling-group named serverdemo"
 aws autoscaling create-auto-scaling-group --auto-scaling-group-name serverdemo --launch-configuration-name ubuntuserver --load-balancer-names gjhaveri --min-size 1 --max-size 5 --desired-capacity $5 --vpc-zone-identifier subnet-b36445d7
 echo "Don't forget to kill everything else jhajek will not help you to get your money!! Boom and out of here..."
-
-
-
-//aws ec2 userfile://install
+aws ec2 run-instances --image-id ami-8b77d1eb --key-name test --security-group-ids sg-b5488acc --instance-type t2.micro --user-data file://install-app1.sh --iam-instance-profile Name=developer
