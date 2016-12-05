eb08d324d5267bf41c80bb29e0327fffd48ac111
gjhaveri <itsmeasgaurav@gmail.com>
2016-12-05 01:38:02 -0600

    Modifying the command for deleting queue

--- destroy-env.sh
+++ destroy-env.sh
@@ -2,15 +2,25 @@
 echo "Welcome to world of destruction"
 echo "Wanna see the power of destruction? Check this out!!"
 echo "Autoscaling group being updated"
-aws autoscaling update-auto-scaling-group --auto-scaling-group-name serverdemo  --min-size 0 --max-size 0 	
+aws autoscaling update-auto-scaling-group --auto-scaling-group-name $1  --min-size 0 --max-size 0
 echo "Killing all instances- Kindly be patient and wait"
 aws ec2 wait instance-terminated
 echo "Load Balancer being Detached"
-aws autoscaling detach-load-balancers --auto-scaling-group-name serverdemo --load-balancer-names gjhaveri
+aws autoscaling detach-load-balancers --auto-scaling-group-name $1 --load-balancer-names $2
 echo "AutoScaling Group Deleted. let's party"
-aws autoscaling delete-auto-scaling-group --auto-scaling-group-name serverdemo --force-delete
+aws autoscaling delete-auto-scaling-group --auto-scaling-group-name $1 --force-delete
 echo "Launch Configuration deleted"
-aws autoscaling delete-launch-configuration --launch-configuration-name ubuntuserver
+aws autoscaling delete-launch-configuration --launch-configuration-name $3
 echo "Load Balancer deleted"
-aws elb delete-load-balancer --load-balancer-name gjhaveri
+aws elb delete-load-balancer --load-balancer-name $2
+echo "Deleting Database"
+aws rds delete-db-instance --skip-final-snapshot --db-instance-identifier $4
+echo "deleting sns topic"
+aws sns delete-topic --topic-arn $5
+echo "deleting sqs queue"
+aws sqs delete-queue --queue-url $6
+echo "deleting s3 bucket 1"
+aws s3 rb s3://$7
+echo "deleting s3 bucket 2"
+aws s3 rb s3://$8
 echo "You saved $ xxx for destroying everything! Thank you for using AWS service"
